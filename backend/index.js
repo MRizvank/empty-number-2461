f940e31c8457895b63d6fb5ccc11efe47f4fc047
Rizvan <rizvankurungattil@gmail.com>
2023-02-24 00:24:33 +0530

    day3

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express=require('express')
+require('dotenv').config()
+const connection=require('./config/db')
+const userRoutes=require('./routes/user.routes')
+
+const app=express()
+
+app.use(express.json())
+
+app.use('/user',userRoutes)
+
+app.get('/',(req,res)=>{
+    res.send("home page")
+})
+
+
+app.listen(process.env.SERVER_PORT,async()=>{
+    try {
+        await connection
+        console.log('connected to db')
+        console.log(`server is running at server http://localhost:${process.env.SERVER_PORT}`)
+    } catch (error) {
+        console.log('cannot connect to db',error.message)
+    }
+   
+})
