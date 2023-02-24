a5c9a74254bebb83df0c22250a2478f571dd54e9
Rizvan <rizvankurungattil@gmail.com>
2023-02-24 23:12:01 +0530

    day_4

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express=require('express')
 require('dotenv').config()
 const connection=require('./config/db')
 const userRoutes=require('./routes/user.routes')
+const productRoutes=require('./routes/product.routes')
 
 const app=express()
 
 app.use(express.json())
 
 app.use('/user',userRoutes)
+app.use('/product',productRoutes)
 
 app.get('/',(req,res)=>{
     res.send("home page")
